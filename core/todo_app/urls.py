750728a99bad04c1fbf83ca6b4e12af8bed0ca36
anfkekkek234 <160501730+anfkekkek234@users.noreply.github.com>
2024-11-29 11:19:00 +0330

    Update urls.py

--- core/todo_app/urls.py
+++ core/todo_app/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('update/<int:pk>/', views.TaskUpdateView.as_view(), name='task_edit'),
     path('delete/<int:pk>/', views.TaskDeleteView.as_view(), name='task_delete'),
     path('task/<int:pk>/mark_done/', views.MarkDoneView.as_view(), name='mark_done'),
-    path('api/v1/', include('todo_app.api.v1.urls'))  # اتصال به urls مربوط به api
+    path('api/v1/', include('todo_app.api.v1.urls'))  
 ]
